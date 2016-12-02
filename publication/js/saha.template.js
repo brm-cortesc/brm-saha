c965302b2e6190f4b49585e8b999db8e291d927f
Cristian Cortés <cristian.cortes@brm.com.co>
2016-12-01 23:16:03 -0500

    templatejs

---
+++ publication/js/saha.template.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+  $('.commerce-product-field-field-images').unwrap().unwrap();
+  $('.commerce-product-field-commerce-price, .field-name-body, .commerce-product-extra-field, .commerce-add-to-cart').wrapAll('<div class="info-producto" />');
+});
+
+//# sourceMappingURL=../maps/saha.template.js.map
