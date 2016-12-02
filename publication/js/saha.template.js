dfd49a21aeb73ab079145af15623fa3101c800bc
Cristian Cortés <cristian.cortes@brm.com.co>
2016-12-02 01:35:17 -0500

    responsive

--- publication/js/saha.template.js
+++ publication/js/saha.template.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($) {
   $('.commerce-product-field-field-images').unwrap().unwrap();
   $('.commerce-product-field-commerce-price, .field-name-body, .commerce-product-extra-field, .commerce-add-to-cart').wrapAll('<div class="info-producto" />');
+  if ($(window).width() < 768) {
+    $('.info-producto, .commerce-product-field-field-images').wrapAll('<div class="producto" />');
+  }
 });
 
 //# sourceMappingURL=../maps/saha.template.js.map
