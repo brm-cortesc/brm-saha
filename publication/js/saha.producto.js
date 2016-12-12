5c7d759b96d6696b4f469d07b6a4ee73297bf693
Cristian Cortés <cristian.cortes@brm.com.co>
2016-12-12 16:21:19 -0500

    slider de producto

--- publication/js/saha.producto.js
+++ publication/js/saha.producto.js
@@ -31,15 +31,6 @@ jQuery(document).ready(function($) {
   };
 
   /* Sliders */
-  if ($(window).width() >= 768) {
-    $('.slider-vista-top').bxSlider(sliderDesktop);
-    $('.slider-vista-bottom').bxSlider(sliderDesktop);
-    $('.slider-vista').bxSlider(sliderDesktop);
-  } else {
-    $('.slider-vista-top').bxSlider(sliderMobile);
-    $('.slider-vista-bottom').bxSlider(sliderMobile);
-    $('.slider-vista').bxSlider(sliderMobile);
-  }
 
   /*cambio de vista del producto */
 
